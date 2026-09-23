90cfa3af94df9920108b75a45105f4175ef0d114
Paul T <pault12@gmail.com>
2026-09-23 23:41:32 +0000

    fixed a little bug saving to the library

--- ROMAK/save.pl
+++ ROMAK/save.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/perl 
 
-my $filename = `./lll nano "Print the 15 chars filename Use only alphanumeric and no spaces to reflect the summary of this <<1>> DO NOT USE THE WORD SUMMARY or 15 or CHARS" SPOOL/output.out 2>&1`; chomp $filename;
+my $head15 = `head -15 LAST/output.out > head15.txt`;
+
+my $filename = `./lll mini "Print the 15 chars filename Use only alphanumeric and no spaces to reflect the summary of this <<1>> DO NOT USE THE WORD SUMMARY or 15 or CHARS" head15.txt 2>&1`; chomp $filename;
 
 $filename .= ".txt";
 
-system "cp SPOOL/output.out LIB/$filename";
+system "cp LAST/output.out LIB/$filename";
 
 print "$filename - Saved!\n";
 
